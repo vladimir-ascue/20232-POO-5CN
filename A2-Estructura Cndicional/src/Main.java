8aa793e47dbf68d447fc77e27e066f5b24967561
vladimir-ascue <vladimir.ascue@gmail.com>
2023-02-16 18:30:29 -0500

    A02 - Condicionales

---
+++ A2-Estructura Cndicional/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        int edad = 18;
+//        if( edad < 18 ){
+//            System.out.println("ELERTA ... FUGA.. FUGA ... FUGA ....");
+//        }else{
+//            System.out.println("Con fe no mas ...");
+//        }
+        // CASESENSITYVE =
+        String nombre = "Karen";
+        // if( nombre == "Karen"){
+        // if(nombre.compareTo("karen") == 0){
+        // Recomendada por el profe
+//        if(nombre.equals("Karen")){
+//            System.out.println(" :'( ");
+//        }else{
+//            System.out.println(" :P ");
+//        }
+//
+//        String dato = "";
+//
+//        if ( dato.equals("")){
+//            System.out.println("Es una cadena vacia");
+//        }else{
+//            System.out.println("No es una cadena vacia");
+//        }
+        System.out.println("*******************");
+        System.out.print("Ingrse su edad: "); // Promt >> Mensaje para indicar que hacer
+        int edad = new Scanner(System.in).nextInt();
+        // 17 > Debes sacara tu LM
+        // 18 - 20 > Te pasaste sera ultado
+        // 21 a más > Eres omiso irás a prision
+        if( ( edad >  0 ) && ( edad < 17 ) ){ // ||
+            System.out.println("Eres mu chibolo(a)");
+        }else{
+            if(edad == 17){
+                System.out.println("Debes sacara tu LM");
+            }else{
+                if( edad <= 20 ){
+                    System.out.println("Te pasaste sera ultado");
+                }else{
+                    System.out.println("Eres omiso irás a prision");
+                }
+            }
+        }
+    }
+}
